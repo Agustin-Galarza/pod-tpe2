0723ef9fb6ba138f5ac901e31bb433adc25a82b0
Agustin Galarza <agalarza@itba.edu.ar>
2023-05-31 10:16:32 -0300

    Optimized Collators.
    Moved lambdas to class methods.
    Moved IMaps as class fields.

--- api/src/main/java/ar/edu/itba/pod/aggregation/collators/Query1Collator.java
+++ api/src/main/java/ar/edu/itba/pod/aggregation/collators/Query1Collator.java
@@ -2,41 +2,41 @@ package ar.edu.itba.pod.aggregation.collators;
 
 import ar.edu.itba.pod.model.Station;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.HazelcastInstanceAware;
 import com.hazelcast.core.IMap;
 import com.hazelcast.mapreduce.Collator;
 
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.function.ToIntFunction;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 @SuppressWarnings("deprecation")
 public class Query1Collator implements Collator<Map.Entry<Integer,Integer>, List<Map.Entry<String,Integer>>> {
-    private final String stationsMapName;
-    private HazelcastInstance hazelcastInstance;
+    private final IMap<Integer, Station> stationsMap;
     public Query1Collator(HazelcastInstance hazelcastInstance, String stationsMapName){
-        this.stationsMapName = stationsMapName;
         // Since the Collator is executed on the job emitting member (the client in this case) its of no use to
         // implement HazelcastInstanceAware and the instance needs to be passed here.
-        this.hazelcastInstance = hazelcastInstance;
+        this.stationsMap = hazelcastInstance.getMap(stationsMapName);
     }
+
+    private String stationNameMapper(int id){
+        return stationsMap.get(id).name();
+    }
+    private int tripsAmount(Map.Entry<String,Integer> entry){
+        return entry.getValue();
+    }
+
+    private String alphabetical(Map.Entry<String,Integer> entry){
+        return entry.getKey();
+    }
+
     @Override
     public List<Map.Entry<String,Integer>> collate(Iterable<Map.Entry<Integer,Integer>> values) {
-        final IMap<Integer, Station> stationsMap = hazelcastInstance.getMap(stationsMapName);
-        final Function<Integer, String> stationNameMapper = id -> stationsMap.get(id).name();
-
-        final ToIntFunction<Map.Entry<String,Integer>> tripsAmount = Map.Entry::getValue;
-        final Function<Map.Entry<String,Integer>, String> alphabetical = Map.Entry::getKey;
         // ordenar los valores, primero por cant de viajes luego alfabético por nombre de estación
         return StreamSupport.stream(values.spliterator(), true)
                 .filter(entry -> stationsMap.containsKey(entry.getKey()))
-                .map(entry -> Map.entry(stationNameMapper.apply(entry.getKey()), entry.getValue()))
-                .sorted(Comparator.comparingInt(tripsAmount).thenComparing(alphabetical))
+                .map(entry -> Map.entry(stationNameMapper(entry.getKey()), entry.getValue()))
+                .sorted(Comparator.comparingInt(this::tripsAmount).reversed().thenComparing(this::alphabetical))
                 .toList();
     }
 
--- api/src/main/java/ar/edu/itba/pod/aggregation/collators/Query2Collator.java
+++ api/src/main/java/ar/edu/itba/pod/aggregation/collators/Query2Collator.java
@@ -4,26 +4,23 @@ import ar.edu.itba.pod.model.BikeRental;
 import ar.edu.itba.pod.model.Station;
 import ar.edu.itba.pod.utils.Pair;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.HazelcastInstanceAware;
 import com.hazelcast.core.IMap;
 import com.hazelcast.mapreduce.Collator;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.ToDoubleFunction;
 import java.util.stream.StreamSupport;
 
 @SuppressWarnings("deprecation")
 public class Query2Collator implements
-        Collator<Map.Entry<Integer, Pair<Double,Integer>>, List<Map.Entry<String,Pair<Double, BikeRental>>>> {
+        Collator<Map.Entry<Integer, Pair<Double, Integer>>, List<Map.Entry<String, Pair<Double, BikeRental>>>> {
     private final String stationsMapName;
     private final String rentalsMapName;
     private final int maxStations;
     private final HazelcastInstance hazelcastInstance;
-
+    private final IMap<Integer, Station> stationsMap;
+    private final IMap<Integer, BikeRental> rentalsMap;
 
     public Query2Collator(HazelcastInstance hazelcastInstance, String stationsMapName, String rentalsMapName, int maxStations) {
         this.stationsMapName = stationsMapName;
@@ -32,42 +29,48 @@ public class Query2Collator implements
         // Since the Collator is executed on the job emitting member (the client in this case) its of no use to
         // implement HazelcastInstanceAware and the instance needs to be passed here.
         this.hazelcastInstance = hazelcastInstance;
+        this.stationsMap = hazelcastInstance.getMap(stationsMapName);
+        this.rentalsMap = hazelcastInstance.getMap(rentalsMapName);
+    }
+
+    private String stationsNameMapper(int id) {
+        return stationsMap.get(id).name();
+    }
+
+    private boolean stationIsPresent(Map.Entry<Integer, Pair<Double, Integer>> tripRecord) {
+        int statStationId = tripRecord.getKey();
+        int rentalId = tripRecord.getValue().right();
+        int endStationId = rentalsMap.get(rentalId).endStationId();
+        return stationsMap.containsKey(statStationId) && stationsMap.containsKey(endStationId);
+    }
+
+    // Searches in stations map and replaces stationId for its name
+    private Map.Entry<String, Pair<Double, Integer>> getStationName(Map.Entry<Integer, Pair<Double, Integer>> tripRecord) {
+        return Map.entry(stationsNameMapper(tripRecord.getKey()), tripRecord.getValue());
+    }
+
+    private double tripSpeed(Map.Entry<String, Pair<Double, Integer>> tripRecord) {
+        return tripRecord.getValue().left();
+    }
+
+    private String alphabetical(Map.Entry<String, Pair<Double, Integer>> entry) {
+        return entry.getKey();
+    }
+
+    // Searches in rentals map and replaces tripId for its BikeRental
+    private Map.Entry<String, Pair<Double, BikeRental>> getRental(Map.Entry<String, Pair<Double, Integer>> tripRecord) {
+        return Map.entry(tripRecord.getKey(), new Pair<>(tripRecord.getValue().left(), rentalsMap.get(tripRecord.getValue().right())));
     }
 
     @Override
-    public List<Map.Entry<String, Pair<Double, BikeRental>>> collate(Iterable<Map.Entry<Integer, Pair<Double, Integer>>> values) {
-        final IMap<Integer, Station> stationsMap = hazelcastInstance.getMap(stationsMapName);
-        final IMap<Integer, BikeRental> rentalsMap = hazelcastInstance.getMap(rentalsMapName);
-
-        //////////////// Functions ////////////////
-        final Function<Integer, String> stationsNameMapper = id -> stationsMap.get(id).name();
-
-        final Predicate<Map.Entry<Integer, Pair<Double, Integer>>> stationIsPresent =
-                tripRecord -> {
-                    int statStationId = tripRecord.getKey();
-                    int rentalId = tripRecord.getValue().right();
-                    int endStationId = rentalsMap.get(rentalId).endStationId();
-                    return stationsMap.containsKey(statStationId) && stationsMap.containsKey(endStationId);
-                };
-        // Searches in stations map and replaces stationId for its name
-        final Function<Map.Entry<Integer, Pair<Double,Integer>>, Map.Entry<String, Pair<Double,Integer>>>
-                getStationName = tripRecord -> Map.entry(stationsNameMapper.apply(tripRecord.getKey()), tripRecord.getValue());
-        // Searches in rentals map and replaces tripId for its BikeRental
-        final Function<Map.Entry<String, Pair<Double,Integer>> ,Map.Entry<String, Pair<Double,BikeRental>>>
-                getRental = tripRecord -> Map.entry(tripRecord.getKey(), new Pair<>(tripRecord.getValue().left(), rentalsMap.get(tripRecord.getValue().right())));
-        final ToDoubleFunction<Map.Entry<String, Pair<Double,Integer>>> tripSpeed =
-                tripRecord -> tripRecord.getValue().left();
-        final Function<Map.Entry<String, Pair<Double,Integer>>, String> alphabetical =
-                Map.Entry::getKey;
-        ////////////////////////////////////////////////
-
-        // Sort values, fill missing data and limit values to the given amount
+    public List<Map.Entry<String, Pair<Double, BikeRental>>> collate(
+            Iterable<Map.Entry<Integer, Pair<Double, Integer>>> values) {
         return StreamSupport.stream(values.spliterator(), true)
-                .filter(stationIsPresent)
-                .map(getStationName)
-                .sorted(Comparator.comparingDouble(tripSpeed).reversed().thenComparing(alphabetical))
+                .filter(this::stationIsPresent)
+                .map(this::getStationName)
+                .sorted(Comparator.comparingDouble(this::tripSpeed).reversed().thenComparing(this::alphabetical))
                 .limit(maxStations)
-                .map(getRental)
+                .map(this::getRental)
                 .toList();
     }
 
