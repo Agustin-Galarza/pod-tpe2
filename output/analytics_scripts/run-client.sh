962ecc9a15cf6c6d15020b38818ec331ce857aca
Agustin Galarza <agalarza@itba.edu.ar>
2023-05-31 10:49:22 -0300

    Added args to run-client.sh

--- output/analytics_scripts/run-client.sh
+++ output/analytics_scripts/run-client.sh
@@ -1,6 +1,9 @@
 Color_Off='\033[0m'
 BRed='\033[1;31m'
 
+runs=10
+query2_output_lines=40
+
 curr_dir=$(pwd)
 tmp_dir="$curr_dir"/tmp
 
@@ -16,14 +19,14 @@ cp -f "$curr_dir"/stations.csv "$client_dir"
 
 addresses=""
 
-usage() { echo "Usage: $0 -a <str> (list of node addresses)" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -a <str> (list of node addresses) [-r <int> = $runs (client runs)] [-l <int> = $query2_output_lines (output lines in query2)]" 1>&2; exit 1; }
 
-while getopts "ha:" o; do
+while getopts "ha:r:l:" o; do
     case "${o}" in
         a)
             addresses=${OPTARG} || usage
             ;;
-        h)
+        *)
             usage
             ;;
     esac
@@ -31,7 +34,7 @@ done
 
 # Launch client
 cd "$client_dir" || (echo 'error on cd to tmp' && exit 1)
-for i in {1..10}; do
+for i in $(eval echo "{1..$runs}"); do
   echo "######################### Cliente $i #########################"
   # Query 1
   "$client_dir"/query1 -Daddresses="$addresses" -DinPath=. -DoutPath=.;
@@ -41,7 +44,7 @@ for i in {1..10}; do
     exit 1
   fi
   # Query 2
-  "$client_dir"/query2 -Daddresses="$addresses" -DinPath=. -DoutPath=. -Dn=40;
+  "$client_dir"/query2 -Daddresses="$addresses" -DinPath=. -DoutPath=. -Dn="$query2_output_lines";
   res="$?"
   if [ "$res" -ne 0 ];then
     echo -e "$BRed""Error with query 2""$Color_Off"
