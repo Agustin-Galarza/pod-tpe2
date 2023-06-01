7d30dbc97cd8157b6fa43f60a4c0f48673ed047d
Agustin Galarza <agalarza@itba.edu.ar>
2023-06-01 04:04:18 -0300

    Renamed scripts directory.

--- output/analytics_scripts/import_stations_csv.sh
+++
@@ -1,9 +0,0 @@
-# usage: ./import_stations_csv.sh username
-
-user=$1
-path=../../../it.itba.edu.ar/pub/pod/stations.csv
-curr_dir=$(pwd)
-
-scp "$user"@pampero.itba.edu.ar:"$path" /dev/stdout > "$curr_dir"/stations.csv
-
-
